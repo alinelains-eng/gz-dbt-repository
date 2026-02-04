12e79152d7d0cd58d17f4900613f4d8ff3c3c02e
aline.lains@gmail.com <aline.lains@gmail.com>
2026-02-04 18:36:36 +0000

    erreur dans le code

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,12 +1,12 @@
 SELECT 
-    products_id,
-      date_date as date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
-      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin    
-FROM {{ ref('stg_gz_raw_data__sales') }} as s
-LEFT JOIN {{ ref('stg_gz_raw_data__product') }} as p
-ON p.products_id = s.product_id
+    s.products_id,
+    s.date_date,
+    s.orders_id,
+    s.revenue,
+    s.quantity,
+    p.purchase_price,
+    ROUND(s.quantity * p.purchase_price, 2) AS purchase_cost,
+    ROUND(s.revenue - (s.quantity * p.purchase_price), 2) AS margin    
+FROM {{ ref('stg_gz_raw_data__sales') }} AS s
+LEFT JOIN {{ ref('stg_gz_raw_data__product') }} AS p
+    ON p.products_id = s.products_id
