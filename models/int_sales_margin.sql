0a5ca94eba8afd68de507dec3ec6b1bc9078f166
aline.lains@gmail.com <aline.lains@gmail.com>
2026-02-04 17:07:48 +0000

    marjin cost et purchase cost en colonne

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,4 +1,12 @@
-SELECT *    
+SELECT 
+    products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin    
 FROM {{ ref('stg_gz_raw_data__sales') }} as s
 LEFT JOIN {{ ref('stg_gz_raw_data__product') }} as p
 ON p.products_id = s.product_id
